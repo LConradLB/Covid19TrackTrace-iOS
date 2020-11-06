eaae484636c48dc23acc9c028be8b700a0e9d69e
Lewis Luther-Braun <lewis.braun@gmail.com>
2020-11-06 16:34:02 +0000

    Initial Commit

---
+++ CovidTrackAndTrace-iOS/CovidTrackAndTrace_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackAndTrace_iOSApp.swift
+//  CovidTrackAndTrace-iOS
+//
+//  Created by Lewis Luther-Braun on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackAndTrace_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
